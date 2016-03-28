423e80d00e21eda157b205182088a6efde9c556b
Anne Brooks <pharlanne@googlemail.com>
2016-03-28 23:48:15 +0100

    update

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@
 
 
   // *** array of quotes *********
-  var quotes = [{author: "keith", text:"smthing, "},{author: "jay", text:"smthing else, "} ];
+          
+  var quotes = [{author: "keith  ", text:" Are those hand-cut artisan crisps?, "},{author: "jay  ", text:"Visual Basic is the way forward, I don't know why we are doing JavaScript , "} ];
 
   var quoteList = document.getElementById("ql");
   var ul = document.createElement("ul");
@@ -13,9 +14,17 @@
     
     console.log(ul, "creating a ul & appending to ul")
     var li = document.createElement("li");
-    li.innerText = quotes[i]. text +" "+quotes[i].author;
-
+    li.innerText = quotes[i].text +" "+quotes[i].author;
+    var delLink = document.createElement("a");
+    delLink.innerText = "delete";
+    delLink.href = "";
+    li.appendChild(delLink);
     ul.appendChild(li);
+    delLink.onclick = function(event){
+      event.preventDefault();
+      li.remove();
+     
+    }
 
   }
   quoteList.appendChild(ul);
@@ -32,13 +41,25 @@
   var handleClick = function(){
     console.log(input.value);
 
+  
     var li = document.createElement("li");
     li.innerText = input.value;
-
+    var delLink = document.createElement("a");
+    delLink.innerText = "    delete";
+    delLink.href = "";
+    li.appendChild(delLink);
     ul.appendChild(li);
+    delLink.onclick = function(event){
+      event.preventDefault();
+      li.remove();
+     
+    }
 
       // var quoteList = document.getElementById("ql");
       // quoteList.appendChild(ul);
+      input.value = "";
+      var copyText = document.getElementById("copy-text");
+      copyText.innerText = input.value;
       console.log(quoteList);
     }
 
